87671d2ba7bdaaaa3913c3e7739b745685ded3fb
Ben Stein <bumstain@gmail.com>
2018-03-06 02:50:04 +0900

    Created Client class that calls the Facebook Graph API

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -5,5 +5,15 @@ include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe 
 
 Bot.on :message do |message|
-  puts 'got your message!'
+  
+  client = Facebook::Client.new
+  user_data = client.get_user(message.sender["id"])
+  user_name = user_data["first_name"]
+
+  Bot.deliver(
+    recipient: message.sender,
+    message: {
+      text: "Hi #{user_name}"
+    }
+  )
 end
