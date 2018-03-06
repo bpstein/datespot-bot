17aeff1b56b4fd25b268cc930bfc948c544de7fa
Ben Stein <bumstain@gmail.com>
2018-03-06 15:08:15 +0900

    Added User and Log models and expanded Brain logic

--- app/bot/brain.rb
+++ app/bot/brain.rb
@@ -25,4 +25,56 @@ class Brain
   def stop_typing
     Facebook::Client.new.set_typing_off(sender['id'])
   end
+
+  def process_message
+    
+  end
+
+  def process_postback
+    
+  end
+
+  def create_log
+    if message.present? 
+      Log.create(
+        user_id: user.id,
+        fb_message_id: message.id, 
+        message_type: message_type,
+        sent_at: message.sent_at
+      )
+    else 
+      Log.create(
+        user_id: user.id, 
+        message_type: "postback",
+        sent_at: postback.sent_at
+      )
+    end
+  end
+
+  private
+
+  def message_type
+    text.present? ? "text" : attachments.first["type"]
+  end
+
+  def user 
+    @user ||= set_user
+  end
+
+  def set_user
+    @user = User.find_by(fb_id: sender['id'])
+      
+    if @user.nil?
+      fb_user = Facebook::Client.new.get_user(sender["id"])
+      @user = User.create(
+        fb_id: sender["id"],
+        full_name: fb_user["first_name"] + " " + fb_user["last_name"],
+        gender: fb_user["gender"],
+        locale: fb_user["locale"],
+        timezone: fb_user["timezone"]
+      )
+    end
+
+    @user 
+  end
 end
