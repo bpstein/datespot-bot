01fcc5866f8c638790efc711b8b5650ee2fc2481
Ben Stein <bumstain@gmail.com>
2018-07-01 17:57:34 +0900

    Added persistent menu

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -35,8 +35,6 @@ Facebook::Messenger::Thread.set(
 
 # Persistent menu at bottom of chat window
 Facebook::Messenger::Thread.set(
-  setting_type: "call_to_actions",
-  thread_state: "existing_thread",
   persistent_menu: [
     {
       locale: "default",
